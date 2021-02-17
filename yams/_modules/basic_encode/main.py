6fd1f344626820dff64463fe77d6e484cf9265bd
Your Name <you@example.com>
2021-02-17 22:56:08 +0100

    made algo iterative

--- yams/_modules/basic_encode/main.py
+++ yams/_modules/basic_encode/main.py
@@ -1,14 +1,16 @@
+from baseXtoY import baseXtoY
 '''
 gets input, returns coded output
 '''
+
 
 def encode(word):
     codes = [ord(c) for c in word]
     return str(sum(codes[i] * 256 ** i for i in range(len(codes))))
 
-
 def decode(num):
-    if num:
-        return chr(num % 256) + decode(num // 256)
-    else:
-        return ""
+    decoded = ''
+    while num:
+        decoded += chr(num % 256)
+        num = num // 256
+    return decoded
